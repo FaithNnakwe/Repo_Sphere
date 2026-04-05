1aa642fbbb8b7468a9e6b4b8fdda1dedad66735a
knguyen-sudo <lamkhue@outlook.com>
2026-04-04 22:34:24 -0400

    Fix import path for Settings component and add ToastContainer for notifications

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import SetupProfile from "./pages/SetupProfile";
 import DashboardPage from "./pages/DashboardPage";
-import Settings from "./SettingPages/Settings";
+import Settings from "./pages/Settings/Settings";
 
 export default function App() {
   return (
@@ -15,6 +17,7 @@ export default function App() {
         <Route path="/setup-profile" element={<SetupProfile />} />
         <Route path="/settings" element={<Settings />} />
       </Routes>
+      <ToastContainer position="bottom-right" autoClose={3000} />
     </BrowserRouter>
   );
 }
